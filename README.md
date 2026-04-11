02ca0fa28fad0377fae60da87f5c1ebbd98f540d
Rodrigo Chamusca <rchamuscag@gmail.com>
2026-04-11 15:55:28 +0000

    adiciona script de formatação `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do curso.dev para aprender programação - Apoio Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>falaí otária</h1>
+  return <h1>falaí otária</h1>;
 }
 
 export default Home;
