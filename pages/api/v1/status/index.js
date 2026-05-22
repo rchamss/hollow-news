ef4ee33f71b207eb1cbefaec52ed1a4c7da5d6c7
Rodrigo Chamusca <rchamuscag@gmail.com>
2026-05-22 01:36:19 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 3 as sum;');
-  console.log(result.rows)
-  response.status(200).json({ texto: "Ele está vivo!" });
+  const updatedAt = new Date().toISOString();
+  const postgVer = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;")
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB]
+  })
+  const databaseOpenedConnectionsValue = databaseOpenedConnections.rows[0].count
+  console.log(databaseOpenedConnectionsValue)
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgVer.rows[0].server_version,
+        max_connections: maxConnections.rows[0].max_connections,
+        opened_connections: databaseOpenedConnectionsValue,
+      }
+    }
+  });
 }
 
 export default status;
